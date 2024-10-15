64cdbdf163579b07426ed3343d3dbbf26e505471
Kushizaki <kushimegu288@gmail.com>
2024-10-15 11:41:12 +0900

    Promiseでラップした関数の実装

---
+++ 03.asynchronous/promisified_functions.js
@@ -0,0 +1,39 @@
+export function run(db, sql, params = []) {
+  return new Promise(function (resolve, reject) {
+    db.run(sql, params, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        if (sql.includes("INSERT INTO")) {
+          resolve(this.lastID);
+        } else {
+          resolve();
+        }
+      }
+    });
+  });
+}
+
+export function get(db, sql, params = []) {
+  return new Promise(function (resolve, reject) {
+    db.get(sql, params, function (error, row) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+}
+
+export function close(db) {
+  return new Promise(function (resolve, reject) {
+    db.close(function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
