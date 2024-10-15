88a52fd0281f5e8c2805ecd314904d3c96ae4027
Kushizaki <kushimegu288@gmail.com>
2024-10-15 11:38:53 +0900

    db.close前にコールバックを追加

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -3,16 +3,17 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function (error) {
-    db.run("INSERT INTO books(title) VALUES (?)", "JavaScript", function (error) {
+  function () {
+    db.run("INSERT INTO books(title) VALUES (?)", "JavaScript", function () {
       console.log(this.lastID);
       db.get(
         "SELECT * FROM books WHERE title = ?",
         "JavaScript",
         function (error, row) {
           console.log(row);
-          db.run("DROP TABLE books");
-          db.close();
+          db.run("DROP TABLE books", function () {
+            db.close();
+          });
         },
       );
     });
