bb938e2a26ceb63a99115993be31082c7187f8cf
Kushizaki <kushimegu288@gmail.com>
2024-10-16 17:16:55 +0900

    捕捉したいエラーだけを捕捉するよう修正

--- 03.asynchronous/async_await_with_error.js
+++ 03.asynchronous/async_await_with_error.js
@@ -6,24 +6,36 @@ const db = new sqlite3.Database(":memory:");
 asyncAwaitOperationWithError();
 
 async function asyncAwaitOperationWithError() {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
   try {
-    const lastId = await run(db, "INSERT INTO books(title) VALUES (?)");
-    console.log(lastId);
+    await run(
+      db,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    );
+    try {
+      const lastId = await run(db, "INSERT INTO books(title) VALUES (?)");
+      console.log(lastId);
+    } catch (error) {
+      if (error.code === "SQLITE_CONSTRAINT") {
+        console.error(error.message);
+      } else {
+        throw error;
+      }
+    }
+    try {
+      const record = await get(db, "SELECT * FROM table WHERE title = ?", [
+        "JavaScript",
+      ]);
+      console.log(record);
+    } catch (error) {
+      if (error.code === "SQLITE_ERROR") {
+        console.error(error.message);
+      } else {
+        throw error;
+      }
+    }
+    await run(db, "DROP TABLE books");
+    await close(db);
   } catch (error) {
-    console.log(error);
+    console.error(error.message);
   }
-  try {
-    const record = await get(db, "SELECT * FROM table WHERE title = ?", [
-      "JavaScript",
-    ]);
-    console.log(record);
-  } catch (error) {
-    console.log(error);
-  }
-  await run(db, "DROP TABLE books");
-  await close(db);
 }
