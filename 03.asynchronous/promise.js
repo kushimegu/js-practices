65e12d3141f3a34b31da2c0338e31709c48b1be4
Kushizaki <kushimegu288@gmail.com>
2024-10-16 17:15:56 +0900

    Promise形式のリファクタリング

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -3,25 +3,17 @@ import { run, get, close } from "./promisified_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-promiseOperation();
-
-function promiseOperation() {
-  run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  )
-    .then(function () {
-      return run(db, "INSERT INTO books(title) VALUES (?)", ["JavaScript"]);
-    })
-    .then(function (result) {
-      console.log(result);
-      return get(db, "SELECT * FROM books WHERE title = ?", ["JavaScript"]);
-    })
-    .then(function (result) {
-      console.log(result);
-      return run(db, "DROP TABLE books");
-    })
-    .then(function () {
-      return close(db);
-    })
-}
+run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => run(db, "INSERT INTO books(title) VALUES (?)", ["JavaScript"]))
+  .then((lastId) => {
+    console.log(lastId);
+  })
+  .then(() => get(db, "SELECT * FROM books WHERE title = ?", ["JavaScript"]))
+  .then((record) => {
+    console.log(record);
+  })
+  .then(() => run(db, "DROP TABLE books"))
+  .then(() => close(db));
--- 03.asynchronous/promise_with_error.js
+++ 03.asynchronous/promise_with_error.js
@@ -3,35 +3,27 @@ import { run, get, close } from "./promisified_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-promiseOperationWithError();
-
-function promiseOperationWithError() {
-  run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() =>
+    run(db, "INSERT INTO books(title) VALUES (?)")
+      .then((lastId) => {
+        console.log(lastId);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      }),
   )
-    .then(function () {
-      return run(db, "INSERT INTO books(title) VALUES (?)");
-    })
-    .then(function (result) {
-      console.log(result);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {
-      return get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"]);
-    })
-    .then(function (result) {
-      console.log(result);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {
-      run(db, "DROP TABLE books");
-    })
-    .then(function () {
-      return close(db);
-    });
-}
+  .then(() =>
+    get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"])
+      .then((record) => {
+        console.log(record);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      }),
+  )
+  .then(() => run(db, "DROP TABLE books"))
+  .then(() => close(db));
