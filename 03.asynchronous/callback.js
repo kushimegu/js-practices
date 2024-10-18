bbc823481350a718f094fc2bf7e0734c7f054ef7
Kushizaki <kushimegu288@gmail.com>
2024-10-18 14:33:22 +0900

    アロー関数の使用と引数の命名修正

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -4,15 +4,15 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
+  () => {
     db.run("INSERT INTO books (title) VALUES (?)", ["JavaScript"], function () {
       console.log(this.lastID);
       db.get(
         "SELECT * FROM books WHERE title = ?",
         ["JavaScript"],
-        function (error, row) {
+        function (_, row) {
           console.log(row);
-          db.run("DROP TABLE books", function () {
+          db.run("DROP TABLE books", () => {
             db.close();
           });
         },
