d80eb6a53fe628a83fa07e4ac788c62cba89bbc4
Kushizaki <kushimegu288@gmail.com>
2024-10-18 13:14:44 +0900

    try/catchで囲む範囲の修正

--- 03.asynchronous/async_await_with_error.js
+++ 03.asynchronous/async_await_with_error.js
@@ -3,11 +3,11 @@ import { run, get, close } from "./promisified_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 try {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
   try {
     const lastId = await run(db, "INSERT INTO books (title) VALUES (?)");
     console.log(lastId);
@@ -30,8 +30,8 @@ try {
       throw error;
     }
   }
-  await run(db, "DROP TABLE books");
-  await close(db);
 } catch (error) {
   console.error(error.message);
 }
+await run(db, "DROP TABLE books");
+await close(db);
