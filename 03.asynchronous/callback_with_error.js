496495600282052c7ebe11d9e17170478ee1bfd7
Kushizaki <kushimegu288@gmail.com>
2024-10-18 14:43:31 +0900

    アロー関数の使用とエラー箇所でnullを渡すよう修正

--- 03.asynchronous/callback_with_error.js
+++ 03.asynchronous/callback_with_error.js
@@ -4,8 +4,8 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
-    db.run("INSERT INTO books (title) VALUES (?)", function (error) {
+  () => {
+    db.run("INSERT INTO books (title) VALUES (?)", [null], function (error) {
       if (error) {
         console.error(error.message);
       } else {
@@ -20,7 +20,7 @@ db.run(
           } else {
             console.log(row);
           }
-          db.run("DROP TABLE books", function () {
+          db.run("DROP TABLE books", () => {
             db.close();
           });
         },
