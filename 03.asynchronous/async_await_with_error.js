a357ef9dbd2a3b644ed5db1847c7d448bfbfe680
Kushizaki <kushimegu288@gmail.com>
2024-10-21 17:49:54 +0900

    prettierに沿った修正

--- 03.asynchronous/async_await_with_error.js
+++ 03.asynchronous/async_await_with_error.js
@@ -9,7 +9,9 @@ await run(
 );
 try {
   try {
-    const thisObject = await run(db, "INSERT INTO books (title) VALUES (?)", [null]);
+    const thisObject = await run(db, "INSERT INTO books (title) VALUES (?)", [
+      null,
+    ]);
     console.log(thisObject.lastID);
   } catch (error) {
     if (error.code && error.code === "SQLITE_CONSTRAINT") {
--- 03.asynchronous/promise_with_error.js
+++ 03.asynchronous/promise_with_error.js
@@ -7,22 +7,21 @@ run(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
-  .then(() =>
-    run(db, "INSERT INTO books (title) VALUES (?)", [null]))
+  .then(() => run(db, "INSERT INTO books (title) VALUES (?)", [null]))
   .then((thisObject) => {
     console.log(thisObject.lastID);
-    return get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"])
+    return get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"]);
   })
   .catch((error) => {
     console.error(error.message);
-    return get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"])
+    return get(db, "SELECT * FROM table WHERE title = ?", ["JavaScript"]);
   })
   .then((row) => {
     console.log(row);
-    return run(db, "DROP TABLE books")
+    return run(db, "DROP TABLE books");
   })
   .catch((error) => {
     console.error(error.message);
-    return run(db, "DROP TABLE books")
+    return run(db, "DROP TABLE books");
   })
   .then(() => close(db));
