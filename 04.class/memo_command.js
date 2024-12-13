032928a19d284184c79bd4012f58cb7809b54cc0
Kushizaki <kushimegu288@gmail.com>
2024-12-13 16:08:08 +0900

    オプションが有効か確認するメソッドのリファクタリング

--- 04.class/memo_command.js
+++ 04.class/memo_command.js
@@ -33,12 +33,12 @@ export default class MemoCommand {
 
   #checkOption() {
     const validOptions = ["-l", "-r", "-d"];
-    if (this.#argv.length === 0) {
-      return;
-    } else if (
-      this.#argv.length >= 2 ||
-      !validOptions.includes(this.#argv[0])
+    if (
+      this.#argv.length === 0 ||
+      (this.#argv.length === 1 && validOptions.includes(this.#argv[0]))
     ) {
+      return;
+    } else {
       throw new Error("l、r、dからオプションを一つだけ指定してください。");
     }
   }
